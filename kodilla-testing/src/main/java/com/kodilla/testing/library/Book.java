dea72f779b76851d7f3001213828617b01659a0c
fmisiarz <f.misiarz@o2.pl>
2020-08-02 16:54:19 +0200

    zad66

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/Book.java
@@ -0,0 +1,54 @@
+package com.kodilla.testing.library;
+
+public class Book {
+    String author;
+    String title;
+    int publicationYear;
+
+    public Book(String author, String title, int publicationYear) {
+        this.author = author;
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (publicationYear != book.publicationYear) return false;
+        if (!author.equals(book.author)) return false;
+        return title.equals(book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = author.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + publicationYear;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
+}
