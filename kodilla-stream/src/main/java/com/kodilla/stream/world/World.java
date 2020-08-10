bb8e128ad3829b7da7d1f87e6c4b76007c006025
fmisiarz <f.misiarz@o2.pl>
2020-08-11 00:13:19 +0200

    7.4poprawa

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class World {
-    Continent continent;
-    Country country;
 public List<Continent> ContinentList = new ArrayList<>();
 public BigDecimal getPeopleQuantity(){
     return ContinentList.stream().flatMap(Continent->Continent.CountryList.stream()).map(Country::getPeopleQuantity).reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
