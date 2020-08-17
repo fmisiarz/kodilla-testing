001e95eb8abad1a4d761943ca5f0d414588e783b
fmisiarz <f.misiarz@o2.pl>
2020-08-17 15:46:34 +0200

    CatchProblem

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,4 +1,22 @@
 package com.kodilla.exception.io;
 
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class FileReader {
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("File/names.txt").getFile());
+        Path path = Paths.get(file.getPath());
+        try {
+            Stream<String> fileLines = Files.lines(path);
+        } catch (IOException e) {
+            System.out.println("OH NO ERROR ERROR !!!!");
+        }
+    }
 }
